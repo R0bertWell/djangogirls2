ef2e305ebb70227521a481a0e4ccc31899bad1b1
r0bertwell <r0bertwell@hotmail.com>
2021-03-16 17:24:54 -0300

    Ajeitando o blog usando grid-XD

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_detail.html', {'posts': posts, 'post': post})
 
 
 def post_new(request):
